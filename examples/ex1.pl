4c2559ae020eb6862c47f6a71f9a247cc9f050e9
Michal Špaček <tupinek@gmail.com>
2013-03-28 16:07:17 +0100

    Add examples.

---
+++ examples/ex1.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Error::Pure::HTTP::Error qw(err);
+
+# Error.
+err '1';
+
+# Output:
+# Content-type: text/plain
+# 
+# #Error [/home/skim/oot:12] 1
