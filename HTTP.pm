8d77f68eb26ad19d1ace346d0e4079c2a51b2f69
Michal Špaček <tupinek@gmail.com>
2013-05-21 10:12:09 +0200

    Prepare for 0.12 version.

--- HTTP.pm
+++ HTTP.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 # Version.
-our $VERSION = 0.11;
+our $VERSION = 0.12;
 
 1;
 
@@ -73,6 +73,6 @@ BSD license.
 
 =head1 VERSION
 
-0.11
+0.12
 
 =cut
--- HTTP/Error.pm
+++ HTTP/Error.pm
@@ -16,7 +16,7 @@ Readonly::Array our @EXPORT_OK => qw(err);
 Readonly::Scalar my $EVAL => 'eval {...}';
 
 # Version.
-our $VERSION = 0.11;
+our $VERSION = 0.12;
 
 # Ignore die signal.
 $SIG{__DIE__} = 'IGNORE';
@@ -145,6 +145,6 @@ BSD license.
 
 =head1 VERSION
 
-0.11
+0.12
 
 =cut
