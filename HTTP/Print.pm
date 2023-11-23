bab34946dfeb39d7801ea8443534f23ca697ddae
Michal Josef Špaček <tupinek@gmail.com>
2023-11-23 19:16:51 +0100

    Improve LICENSE AND COPYRIGHT sections in doc

--- HTTP/Print.pm
+++ HTTP/Print.pm
@@ -145,8 +145,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2012-2018 Michal Josef Špaček
- BSD 2-Clause License
+© 2012-2018 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
